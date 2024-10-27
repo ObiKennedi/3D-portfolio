7d2ebb46b939da5649c5369a6df3ae6bc233cf36
ObiKennedi <166854306+ObiKennedi@users.noreply.github.com>
2024-10-27 02:34:05 +0100

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./sections/Hero"
+import Navbar from "./sections/Navbar"
+import About from "./sections/About"
+
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+      <About/>
+    </main>
+  )
+}
+
+export default App 
